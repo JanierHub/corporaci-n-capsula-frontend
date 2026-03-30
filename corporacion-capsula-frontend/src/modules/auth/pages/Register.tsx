e48d762d6aa074839e3115a932a7b33373cf4250
Janier <Janierf.quinonesq@konradlorenz.edu.co>
2026-03-29 23:37:23 -0500

    eliminación prueba

--- corporacion-capsula-frontend/src/modules/auth/pages/Register.tsx
+++ corporacion-capsula-frontend/src/modules/auth/pages/Register.tsx
@@ -3,8 +3,5 @@ import RegisterForm from "../components/RegisterForm"
 const Register = () => {
   return <RegisterForm />
 }
-const Login = () => {
-  return <h1 style={{ color: "white" }}>LOGIN FUNCIONA</h1>
-}
 
 export default Register
